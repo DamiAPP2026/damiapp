136343bc36d256eae2215545b5908e9ea0bdd43f
DamiAPP2026 <damianoferri2020@gmail.com>
2026-04-09 23:14:35 +0200

    ver xx.xx.18

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import DoctorView from './DoctorView'
 
 const PIN_REALE = '261120'
 const PIN_DEMO = '010101'
-const VERSION = '05.00.16'
+const VERSION = '05.00.18'
 
 const f = (base) => `${Math.round(base * 1.15)}px`
 
